590c18529fd27f5f7bb5a2b07bd2e0a384d9d285
winnxl <23469503+winnxl@users.noreply.github.com>
2022-01-03 15:16:26 -0500

    Update Tilt_Nano.ino

--- SensorModule/Tilt_Nano/Tilt_Nano.ino
+++ SensorModule/Tilt_Nano/Tilt_Nano.ino
@@ -6,52 +6,49 @@
 SoftwareSerial MyBlue(10, 11); // (TX, RX on HC-05)
 
 const int  sensorPin = 4;    // Tilt Sensor
-const int ledPin = 3;       // External LED pin
+//const int ledPin = 13;       // LED pin (13 is onboard)
 
-int lastRead = 1;   // Tilt Switch Read 1 = HIGH (Default if sitting right side up)
+int lastRead = 1;   // 1 = HIGH (Tilt Sensor is standing up)
 unsigned int totalCounter = 0;    // 0 to 65,535
-int delayCounter = 0; // Loops 0-9 for 50 ms delay
+int loopDelay = 50; // delay at the end of each loop()
+int transmitDelay = 500; // BT Module transmission delay - data gets corrupted/congested if too low
+int transmitCooldown = transmitDelay;
 
 void setup() {
   // inits
   pinMode(sensorPin, INPUT);
-  pinMode(ledPin, OUTPUT);
+  //  pinMode(ledPin, OUTPUT);
 
-  Serial.begin(9600); // Serial Monitor
+  //  Serial.begin(9600); // Serial Monitor
   MyBlue.begin(9600);
 
-  Serial.println("Password is 1234 or 000");
-  digitalWrite(ledPin, HIGH);
-
-  if (MyBlue.isListening()) {
-    Serial.write("Is Listening\n");
-  }
+  //  Serial.println("Ready to connect\nDefualt password is 1234 or 000");
+  //  if (MyBlue.isListening()) {
+  //    Serial.write("Is Listening\n");
+  //  }
 }
 
 
 void loop()
 {
-  // Send new total every half second
-  if (delayCounter >= 9 ) {
-    //int w = MyBlue.println(totalCounter);
+  // Transmit Total
+  if (transmitCooldown <= 0) {
     MyBlue.println(totalCounter);
+    transmitCooldown = transmitDelay;
   }
 
-  //Switch
+  // Register Tilts
   if (digitalRead(sensorPin) == LOW && lastRead == 1) {
-    totalCounter++;
-
-    //    Serial.print("Total Cycled: ");
-    //    Serial.println(totalCounter);
-    //    digitalWrite(ledPin, LOW);
     lastRead = 0;
+    totalCounter++;
+    //Serial.println(totalCounter);
+    //digitalWrite(ledPin, LOW);
   }
   else if (digitalRead(sensorPin) == HIGH && lastRead == 0) {
-    //digitalWrite(ledPin, HIGH);
-    analogWrite(ledPin, 63); // 0-255
     lastRead = 1;
+    //digitalWrite(ledPin, HIGH);
   }
 
-  delayCounter = (delayCounter + 1) % 10; // If delay of 50, resets to 0 every half second
-  delay(50);
+  transmitCooldown -= loopDelay;
+  delay(loopDelay);
 }
