c0fbbb4275780f6b22405d8c1de670cc8567b3ca
winnxl <23469503+winnxl@users.noreply.github.com>
2022-02-17 00:43:09 -0500

    Update preload.js

--- Visualizer/src/preload.js
+++ Visualizer/src/preload.js
@@ -1,7 +1,30 @@
 // All Node.js APIs are available in the preload process.
 const { contextBridge, ipcRenderer } = require('electron')
+const Chart = require('chart.js');
 
 // IPC for switching pages
-contextBridge.exposeInMainWorld('electronAPI', {
-    switchPage: (mssg) => ipcRenderer.send('switchPage', mssg)
+contextBridge.exposeInMainWorld('electronIPC', {
+  switchPage: (mssg) => ipcRenderer.send('switchPage', mssg)
 })
+
+// Chart.js
+let ctx;
+let chart;
+contextBridge.exposeInMainWorld(
+  'electronChart',
+  {
+    createChart: (id, params) => {
+      ctx = document.getElementById(id);
+      chart = new Chart(ctx, params);
+    },
+    updateChart: (xLabels, datasetsLabel, datasetsData, datasetsBorderColor) => {
+      chart.data.labels = xLabels;
+      for (let i = 0; i < datasetsLabel.length; i++) {
+        chart.data.datasets[i].label = datasetsLabel[i];
+        chart.data.datasets[i].data = datasetsData[i];
+        chart.data.datasets[i].borderColor = datasetsBorderColor[i];
+      }
+      chart.update();
+    }
+  }
+)
